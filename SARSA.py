065d1d4d9b1af5e13d9ef3a7038b518fa2439561
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-05 16:41:10 -0600

    Big dump

--- SARSA.py
+++ SARSA.py
@@ -1,37 +1,79 @@
 import numpy as np
+import time
+# What each action does
+	# 0: accelerate left
+	# 1: Dont accelerate
+	# 2: accelerate right
 
-def Learn(env, numEpisodes=1, epsilon=0.1):
+# nextStateMetrics [x-axis (-1.2 to 0.6), velocity(-0.07 to 0.07)]
 
-	table = np.zeros((2, 2)) #np.zeros((env.observation_space.n, env.action_space.n))
+def Learn(env, numEpisodes=10, epsilon=0.9, alpha=0.1, gamma=0.99):
+
+	
+
+	stateTable = np.zeros((8250, 3)) 
+
+	# initialize high score
+	HighScoreX = 0
+	HighScoreVel = 0
 
 	for episode in range(numEpisodes):
+		
+		print("\n\n\n NEW EPISODE \n\n\n")
+		time.sleep(1)
 
-		state, info = env.reset(seed=42)
+		epsilon -= epsilon*0.2
+		print(epsilon)
+		stateMetrics, info = env.reset(seed=42)
+
+		# get initial state and action
+		state = (round(stateMetrics[0]*100)+45) * abs(round(stateMetrics[1]*1000))
+		action = policy(stateTable, state, epsilon)
+		print(f"\n pos {round(stateMetrics[0]*100)+45} velo: {abs(round(stateMetrics[1]*1000))} result: {state}")
+
 
 		for _ in range(1000):
 
-			# 0: accelerate left
-			# 1: Dont accelerate
-			# 2: accelerate right
-			action = policy(table, state, epsilon, env) # env.action_space.sample()  # this is where you would insert your policy
+
 			
-			# state [x-axis, velocity]
-			nextState, reward, terminated, truncated, info = env.step(action)
-			print(f"a: {action}, ns: {state}, r: {reward}, i: {info}")
+			# take action
+			nextStateMetrics, reward, terminated, truncated, info = env.step(action)
 
+			if nextStateMetrics[0] > HighScoreX:
+				HighScoreX = nextStateMetrics[0]
+			if nextStateMetrics[1] > HighScoreVel:
+				HighScoreVel = nextStateMetrics[1]
 
+			# change reward to be posative for movement
+			reward += abs(nextStateMetrics[1]) * 100 + abs(nextStateMetrics[0])  
+
+			# gets the state as a result
+			nextState = (round(nextStateMetrics[0]*100)+45) * abs(round(nextStateMetrics[1]*1000))
+
+			# get the next action
+			nextAction = policy(stateTable, state, epsilon)
+
+			print(f"\n pos {round(stateMetrics[0]*100)+45} velo: {abs(round(stateMetrics[1]*1000))} result: {state}")
+
+			# q_table[state, action] += alpha * (reward + gamma * q_table[next_state, next_action] - q_table[state, action])
+
+			stateTable[state, action] += alpha*(reward+gamma*stateTable[nextState, nextAction] - stateTable[state, action])
+			
 			state = nextState
+			action = nextAction
+
 			if terminated or truncated:
+				print(f"\n\n GAME OVER FOR \nterminated: {terminated}\ntruncated: {truncated}")
+				print(f"\n HighScoreX: {HighScoreX}\n HighScoreVel: {HighScoreX}")
 				break
 				
-	return table
+	return stateTable
 
 # epsilon greedy
-def policy(table, state, epsilon, env):
+def policy(stateTable, state, epsilon):
 	if np.random.rand() > epsilon:
-		print("\nfollow")
+		print(f"\nfollow ")
+		return np.random.choice(len(stateTable[state]))
 	else:
-		print("\nexplore")
-
-	action = env.action_space.sample()
-	return action
+		print(f"\nexplore")
+		return np.argmax(stateTable[state])	
--- gymnasiumTest.py
+++ gymnasiumTest.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 import SARSA
 env = gym.make("MountainCar-v0", render_mode="human")
 
-
 SARSA.Learn(env)
 
 env.close()
