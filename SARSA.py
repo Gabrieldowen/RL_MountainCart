e5e5821fe96dc72c1fac0e1121ce435d1b32e0cb
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-05 11:48:42 -0600

    add epsilon greedy policy

--- SARSA.py
+++ SARSA.py
@@ -1,26 +1,37 @@
 import numpy as np
 
-def Learn(env, numEpisodes=1):
+def Learn(env, numEpisodes=1, epsilon=0.1):
 
 	table = np.zeros((2, 2)) #np.zeros((env.observation_space.n, env.action_space.n))
 
 	for episode in range(numEpisodes):
 
-		observation, info = env.reset(seed=42)
+		state, info = env.reset(seed=42)
 
 		for _ in range(1000):
 
 			# 0: accelerate left
 			# 1: Dont accelerate
 			# 2: accelerate right
-			action = env.action_space.sample()  # this is where you would insert your policy
+			action = policy(table, state, epsilon, env) # env.action_space.sample()  # this is where you would insert your policy
 			
-			# observation [x-axis, velocity]
-			observation, reward, terminated, truncated, info = env.step(action)
-			print(f"a: {action}, o: {observation}, r: {reward}, i: {info}")
+			# state [x-axis, velocity]
+			nextState, reward, terminated, truncated, info = env.step(action)
+			print(f"a: {action}, ns: {state}, r: {reward}, i: {info}")
 
+
+			state = nextState
 			if terminated or truncated:
 				break
 				
 	return table
 
+# epsilon greedy
+def policy(table, state, epsilon, env):
+	if np.random.rand() > epsilon:
+		print("\nfollow")
+	else:
+		print("\nexplore")
+
+	action = env.action_space.sample()
+	return action
