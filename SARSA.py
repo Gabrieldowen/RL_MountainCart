40e87f750955f96e51405b0f3c4737dad4adc9a5
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-02-22 14:45:27 -0600

    add comments

--- SARSA.py
+++ SARSA.py
@@ -23,40 +23,45 @@ def Learn(env, numEpisodes=100, epsilon=1, alpha=0.1, gamma=0.99):
 
 	for episode in range(numEpisodes):
 		
+		# extra items
 		if env.render_mode == 'human':
 			print(f"\n\n\n NEW EPISODE {episode} \n\n\n current epsilon: {epsilon}")
 			print(f"\n totalHighScoreX: {HighScoreX}\n totalHighScoreVel: {HighScoreX} sizeofStates: {np.count_nonzero(stateTable)}")
 
 			time.sleep(1)
-
-		epsilon =  1 - (episode / numEpisodes)
-		print(epsilon)
-		stateMetrics, info = env.reset(seed=42)
-
-		# get initial state and action
-		state = (round(stateMetrics[0]*1000)+45) * abs(round(stateMetrics[1]*1000))
-		action, policyChoice = policy(stateTable, state, epsilon)
-
-		if env.render_mode == 'human':
-			print(f"\n pos {round(stateMetrics[0]*100)+45} velo: {abs(round(stateMetrics[1]*2000))} result: {state}")
-
 		episodeHighScoreX = 0
 		episodeHighScoreVel = 0
 		victory = False
+
+		# update epsilon for next episode
+		epsilon =  1 - (episode / numEpisodes)
+
+		# reset the game for the new episode
+		stateMetrics, info = env.reset(seed=42)
+
+		# get initial state and action
+		state = int(str(round(stateMetrics[0]*100)+45) + str(abs(round(stateMetrics[1]*200))))
+		action, policyChoice = policy(stateTable, state, epsilon)
+
 
 
 		for _ in range(1000):
 
 
 			
-			# take action
+			# take action observe reward and nextState
 			nextStateMetrics, reward, terminated, truncated, info = env.step(action)
 
-			
-			# gets the state as a result
+			# adjust/scale the next state values
 			x = round(nextStateMetrics[0]*100)+45
-			velocity = abs(round(nextStateMetrics[1]*1000))
-			nextState = x * velocity
+			velocity = abs(round(nextStateMetrics[1]*200))
+			nextState = int(str(x) + str(velocity))
+
+			# adjust the reward
+			if x > 0:
+				reward += nextStateMetrics[1] + nextStateMetrics[0]
+			else:
+				reward += nextStateMetrics[0] + abs(nextStateMetrics[0])
 
 			# Keeps total highscores
 			if x > HighScoreX:
@@ -70,24 +75,18 @@ def Learn(env, numEpisodes=100, epsilon=1, alpha=0.1, gamma=0.99):
 			if velocity > episodeHighScoreVel:
 				episodeHighScoreVel = velocity
 
-			# adjust the reward
-			if x > 0:
-				reward += (velocity*0.5) + (abs(x)*1.5)
-			else:
-				reward += (velocity*0.5)  + (abs(x))
 
-
-
-			# get the next action
+			# get the next action from epsilon greedy policy
 			nextAction, policyChoice = policy(stateTable, state, epsilon)
 
 			if env.render_mode == 'human':
-				print(f"\n ep: {episode+1} policy: {policyChoice} pos: {x} velo: {velocity} result: {state} print: {nextAction}")
+				print(f"\n ep: {episode+1} policy: {policyChoice} pos: {x} velo: {velocity} state: {state} n/a: {nextAction}")
 
 			# q_table[state, action] += alpha * (reward + gamma * q_table[next_state, next_action] - q_table[state, action])
 
 			stateTable[state, action] += alpha*(reward+gamma*stateTable[nextState, nextAction] - stateTable[state, action])
-			
+
+			# moves to the next state			
 			state = nextState
 			action = nextAction
 
@@ -105,6 +104,13 @@ def Learn(env, numEpisodes=100, epsilon=1, alpha=0.1, gamma=0.99):
 				
 	plotLearning(numEpisodes)
 	return stateTable
+
+# epsilon greedy
+def policy(stateTable, state, epsilon):
+	if np.random.rand() > epsilon:
+		return np.argmax(stateTable[state]), "follow"	
+	else:
+		return np.random.choice([0, 1, 2]), "explore"
 
 def plotLearning(numEpisodes):
 	df = pd.read_csv('results/sarsaResults.csv')
@@ -127,14 +133,14 @@ def plotLearning(numEpisodes):
 	ax1.scatter(victoryRows['episode'], victoryRows['episodeHighScoreX'], color='red', label='Victory')
 	ax1.set_xlabel('episode')
 	ax1.set_ylabel('HighScoreX')
-	ax1.set_title('x learning rate')
+	ax1.set_title('highest x per episode')
 
 	# Plot the second graph
 	ax2.plot(episode, HighScoreVel, color='red')
 	ax2.scatter(victoryRows['episode'], victoryRows['episodeHighScoreVel'], color='blue', label='Victory')
 	ax2.set_xlabel('episode')
 	ax2.set_ylabel('HighScoreVel')
-	ax2.set_title('vel learning rate')
+	ax2.set_title('highest velocity per episode')
 
 	# Adjust layout to prevent overlap
 	plt.tight_layout()
@@ -143,10 +149,4 @@ def plotLearning(numEpisodes):
 	plt.show()
 
 
-# epsilon greedy
-def policy(stateTable, state, epsilon):
-	if np.random.rand() > epsilon:
-		return np.argmax(stateTable[state]), "follow"	
-	else:
-		return np.random.choice([0, 1, 2]), "explore"
 		
